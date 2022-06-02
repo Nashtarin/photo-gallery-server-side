9b63f9d684de548e11279e6a28448a9a11323472
Nashtarin Nur <nashtarin.nur@gmail.com>
2022-06-02 15:23:51 +0600

    100% done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app=express()
-// const { MongoClient } = require('mongodb');
+
 const ObjectId=require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const fileUpload=require('express-fileupload')
@@ -15,7 +15,7 @@ const port=process.env.PORT||5000
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rf3gx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// const bodyParser = require('body-parser');
+
 app.get('/', (req, res) => {
     res.send('Running translator wonderfully')
 });
@@ -24,23 +24,15 @@ async function run(){
         await client.connect();
         const database = client.db('translatorRobberLanguage');
         const photoCollection=database.collection('photos');
-        // const translatedRövarspråkCollection=database.collection('rovarsprak')
+   
         app.get('/photos',async(req,res)=>{
             const cursor= photoCollection.find({});
             const photos=await cursor.toArray();
             res.send(photos)
         })
-        // // app.get('/normal/:textId',async(req,res)=>{
-        // //     const id=req.params.textId;
-        // // const query={query
-        // //            };
-        // // const text=await inputEnglishTextCollection.findOne(query);
-        // // res.json(query)
-        // // })
+  
         app.post('/photos', async (req, res) => {
-            // const photoInput = req.body;
-            //    const result = await photoCollection.insertOne(photoInput);
-            //    res.json(result);})
+          
             const name=req.body.name;
             const email=req.body.email;
             const interest=req.body.interest;
